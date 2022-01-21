80970dd188369314b16f4c9b4e64b3168a26491a
siddhu <siddhudangeti@gmail.com>
2022-01-21 23:46:38 +0530

    security group updated

--- alb.tf
+++ alb.tf
@@ -9,13 +9,13 @@ resource "aws_lb_target_group_attachment" "tg-attach" {
   count            = length(aws_spot_instance_request.ec2-spot)
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
-  port             = 80
+  port             = var.APP_PORT
 }
 
 resource "aws_lb_listener" "alb-listener" {
   count             = var.LB_PUBLIC ? 1 : 0
   load_balancer_arn = data.terraform_remote_state.alb.outputs.alb_public_arn
-  port              = 80
+  port              = var.APP_PORT
   protocol          = "HTTP"
 
   default_action {
