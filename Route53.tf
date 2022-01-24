01a1e3f3ccb0f4850b747217bb77205dcb053acd
siddhu <siddhudangeti@gmail.com>
2022-01-25 00:51:58 +0530

    alb output file is updated

--- Route53.tf
+++ Route53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "private_alb" {
   count = var.LB_PRIVATE
-  zone_id = aws_route53_zone.primary.zod
-  name    = "www.example.com"
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
   type    = "A"
   ttl     = "300"
-  records = [aws_eip.lb.public_ip]
+  records = [data]
 }
