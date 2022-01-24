87fb089537ec32ff0cb33cecf2f1ba92d6e513c9
siddhu <siddhudangeti@gmail.com>
2022-01-25 01:09:53 +0530

    ROute53 file is added for mutable common infra file

--- Route53.tf
+++ Route53.tf
@@ -1,17 +1,17 @@
 resource "aws_route53_record" "private_alb" {
-  count = var.LB_PRIVATE
+  count = var.LB_PRIVATE ? 1 : 0
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "300"
   records = data.terraform_remote_state.alb.outputs.PUBLIC_LB_DNSNAME
 }
 
 #resource "aws_route53_record" "private_alb" {
-#  count = var.LB_PUBLIC
+#  count = var.LB_PUBLIC ? 1 : 0
 #  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
 #  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
-#  type    = "A"
+#  type    = "CNAME"
 #  ttl     = "300"
 #  records = data.terraform_remote_state.alb.outputs.PUBLIC_LB_DNSNAME
 #}
