090b180388eff16bf61b53dd517a03640f5b5b74
siddhu <siddhudangeti@gmail.com>
2022-01-30 08:59:27 +0530

    monitor tag is added for filtering

--- ec2.tf
+++ ec2.tf
@@ -9,19 +9,19 @@ resource "aws_spot_instance_request" "ec2-spot" {
   security_groups = [aws_security_group.sg-ec2.id]
   subnet_id = data.terraform_remote_state.vpc.outputs.private_subnet[count.index]
 }
-resource "aws_ec2_tag" "spot-ec2" {
+resource "aws_ec2_tag" "spot-ec2-name" {
   count       = length(aws_spot_instance_request.ec2-spot)
   resource_id = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   key         = "Name"
   value       = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
 }
-resource "aws_ec2_tag" "spot-ec2" {
+resource "aws_ec2_tag" "spot-ec2-env" {
   count       = length(aws_spot_instance_request.ec2-spot)
   resource_id = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   key         = "ENV"
   value       = var.ENV
 }
-resource "aws_ec2_tag" "spot-ec2" {
+resource "aws_ec2_tag" "spot-ec2-monitor" {
   count       = length(aws_spot_instance_request.ec2-spot)
   resource_id = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
   key         = "monitor"
