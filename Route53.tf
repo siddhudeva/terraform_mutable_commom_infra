09fc4790911f503b62d934ba21d1d93a1d5dcac9
siddhu <siddhudangeti@gmail.com>
2022-02-07 15:33:41 +0530

    jenkins full infra create file update and desabiling filebeat

--- Route53.tf
+++ Route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "private_alb" {
   name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
   type    = "CNAME"
   ttl     = "300"
-  records = data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME[count.index]
+  records = [data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME]
 #  records = data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME
 }
 
@@ -16,3 +16,10 @@ resource "aws_route53_record" "private_alb" {
 #  ttl     = "300"
 #  records = data.terraform_remote_state.alb.outputs.PUBLIC_LB_DNSNAME
 #}
+#resource "aws_route53_record" "private-record" {
+#  count   = var.LB_PRIVATE ? 1 : 0
+#  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+#  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
+#  type    = "CNAME"
+#  ttl     = "300"
+#  records = [data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME]
